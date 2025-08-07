b1eb4810c907ea5b728834a9d6c8a70f5187ea32
Zehra İlayda Demirci <zilaydademirci@gmail.com>
2025-08-07 03:24:34 +0300

    1.2

--- src/model/DatabaseConnection.java
+++ src/model/DatabaseConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
 	private static DatabaseConnection instance;
-	private String username = "hekim";
-	private String password = "hKM.6406";
+	private String username = "**";
+	private String password = "**";
 	private String dbName = "tractordealermanagementsystem";
 	private String host = "127.0.0.1";
 	private int port = 3306;
